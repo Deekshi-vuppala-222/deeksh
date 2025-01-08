605d02aa8bae8311257002433f5fb715224e433e
Deekshi-vuppala-222 <vuppaladeeksh@gmail.com>
2025-01-08 11:52:43 +0530

    assignment

---
+++ loopin.js
@@ -0,0 +1,18 @@
+
+
+   let i=1;
+   let n=5;
+   do{
+       i++;
+       console.log("good moring");
+   }while(i>=n)
+
+   for(let i=1;i<=10;i++){
+       if(i==5)continue;
+       console.log(i);
+       
+   }
+
+   function addToCart(){
+       alert("Product is added to cart");
+   }
