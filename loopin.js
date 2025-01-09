ce0a7584f1da28b8233215f848058bcb2ce05151
Deekshi-vuppala-222 <vuppaladeeksh@gmail.com>
2025-01-09 11:48:34 +0530

    project

--- loopin.js
+++ loopin.js
@@ -1,18 +1,67 @@
 
 
-   let i=1;
-   let n=5;
-   do{
-       i++;
-       console.log("good moring");
-   }while(i>=n)
+//    let i=1;
+//    let n=5;
+//    do{
+//        i++;
+//        console.log("good moring");
+//    }while(i>=n)
 
-   for(let i=1;i<=10;i++){
-       if(i==5)continue;
-       console.log(i);
+//    for(let i=1;i<=10;i++){
+//        if(i==5)continue;
+//        console.log(i);
        
-   }
+//    }
 
-   function addToCart(){
-       alert("Product is added to cart");
-   }
+//    function addToCart(){
+//        alert("Product is added to cart");
+//    }
+
+// function demo(){
+//     console.log("function without parameters");
+// }
+// demo();
+
+//function with paarameters:
+
+// function userDetails(name,age,city){
+//     console.log(name,age,city);
+// }
+// userDetails("deeksh",19,"hyd");
+
+//Anonymous function
+// function(){
+//     console.log("Anonymous function");
+// }
+// ();
+
+//function expression:
+// let x=function(){
+//     console.log("ANonymous");
+// };
+// x();
+
+//IIFE:
+// !Arrow function
+// function demo()
+// {
+//     console.log("Hello");
+// }
+// demo();
+
+// let x=()=>{console.log("Arrow Function");};
+// x();
+//optimized
+// let x=_=>{console.log("Arrow Function");};
+// x();
+
+// let x=(a,b)=>{console.log(a+b);};
+// x(6,5);
+
+let x=_=>{
+    console.log("Hi");
+    console.log("Hello");
+    console.log("Bye");
+
+}
+x();
