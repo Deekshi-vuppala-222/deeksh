8d2ad6de432bd507d247695e72410dc62964767c
Deekshi-vuppala-222 <vuppaladeeksh@gmail.com>
2025-01-21 14:04:58 +0530

    training

---
+++ arrays.js
@@ -0,0 +1,69 @@
+// let user1="Ramya";
+// let user2="Saisree";
+// let user3="deekshitha";
+// let user4="Pallavi";
+// let user5="Divya";
+
+// let users=["Ramya","Saisree","Deekshita","Pallavi","Divya"];
+// // console.log(users);
+// // console.log(users[0]);
+// for(i=0;i<users.length;i++){
+//     console.log(users[i]);  
+// }
+// let x=users.map((user)=>{
+//     //console.log(user);
+//     return user;
+// });
+// console.log(x);
+
+
+// let x=users.forEach((user)=>{
+//     return user;
+// })
+// console.log(x);//undefined
+
+//!create array of objects and print the values:
+
+// let userdetails={
+//     name:"Deeksh",
+//     age:21,
+//     companey:"amazon",
+//     details:function(){
+//     return `My name is ${this.name} and i am working in ${this.companey}`
+// }
+// }
+
+// console.log(userdetails.name);
+// console.log(userdetails.details);
+// console.log(userdetails.details());
+
+// //!create nested object abnd access the property of nested object
+
+// let userdetails={
+//     name:"Deeksh",
+//     age:21,
+//     companey:"amazon",
+//     address:{
+//         city:"Hyd"
+//     }
+    
+// }
+// console.log(userdetails.address.city);
+
+// let userdetails=[{
+
+//!JSON:
+
+// let userdetails={
+//     name:"sam",
+//     age:22,
+//     city:"hyd"
+// }
+// console.log(userdetails);
+
+// let x=JSON.stringify(userdetails);
+// console.log(x);
+
+// let y=JSON.parse(x);
+// console.log(y);
+
